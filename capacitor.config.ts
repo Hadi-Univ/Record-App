f595a9daba92aa3480da75959ba8bb126270e7a6
MMOSHII <94529585+MMOSHII@users.noreply.github.com>
2026-06-03 10:04:16 +0700

    A little config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,19 @@ const config: CapacitorConfig = {
   appId: 'com.hadiuniv.recordapp',
   appName: 'Record App',
   webDir: 'dist',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
-  }
+  },
+  android: {
+    allowMixedContent: true
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '710746488463-b7drnesocml27tih4as8psqo1ib7l339.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 }
 
 export default config
